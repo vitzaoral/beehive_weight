beda0be1ceb268de8116f2aea20e80786ca9af94
vitzaoral@seznam.cz <vitzaoral@seznam.cz>
2020-02-02 19:02:43 +0100

    Update over air (OTA)

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ struct Settings
     const char *wifiSSID = "YYY";
     const char *wifiPassword = "ZZZ";
     const char *blynkAuth = "WWW";
+    const char *version = "1.0.0";
+    const char *firmwareVersionUrl = "http://example.com/version.txt";
+    const char *firmwareBinUrl = "http://example.com/firmware.bin";
 };
 ```
 ### Features
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "HX711.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 #include <BlynkSimpleEsp8266.h>
 #include "../src/settings.cpp"
 
@@ -18,11 +20,120 @@
 const int LOADCELL_DOUT_PIN = D3;
 const int LOADCELL_SCK_PIN = D4;
 
+// 5 minutes
 const double DEEP_SLEEP_TIME = 300e6;
 
 Settings settings;
 
 HX711 scale;
+
+// start OTA update process
+bool startOTA = false;
+
+// Attach Blynk virtual serial terminal
+WidgetTerminal terminal(V3);
+
+// Synchronize settings from Blynk server with device when internet is connected
+BLYNK_CONNECTED()
+{
+  Blynk.syncAll();
+}
+
+// Terminal input
+BLYNK_WRITE(V3)
+{
+  String valueFromTerminal = param.asStr();
+
+  if (String("clear") == valueFromTerminal)
+  {
+    terminal.clear();
+    terminal.println("CLEARED");
+    terminal.flush();
+  }
+  else if (String("update") == valueFromTerminal)
+  {
+    terminal.clear();
+    terminal.println("Start OTA enabled");
+    terminal.flush();
+    startOTA = true;
+  }
+  else if (valueFromTerminal != "\n" || valueFromTerminal != "\r" || valueFromTerminal != "")
+  {
+    terminal.println(String("unknown command: ") + valueFromTerminal);
+    terminal.flush();
+  }
+}
+
+void checkForUpdates()
+{
+  String message = "";
+  String fwVersionURL = settings.firmwareVersionUrl;
+
+  Serial.println("Checking for firmware updates.");
+  Serial.print("Firmware version URL: ");
+  Serial.println(fwVersionURL);
+
+  HTTPClient httpClient;
+  WiFiClient client;
+
+  if (!client.connected())
+  {
+    Serial.print("No iternet connection for update..");
+    return;
+  }
+
+  httpClient.begin(client, fwVersionURL);
+  int httpCode = httpClient.GET();
+
+  if (httpCode == 200)
+  {
+    String newFwVersion = httpClient.getString();
+
+    Serial.print("Current firmware version: ");
+    Serial.println(settings.version);
+    Serial.print("Available firmware version: ");
+    Serial.println(newFwVersion);
+
+    if (String(settings.version) != newFwVersion)
+    {
+      Serial.println("Preparing to update");
+
+      String fwImageURL = settings.firmwareBinUrl;
+      t_httpUpdate_return ret = ESPhttpUpdate.update(client, fwImageURL);
+
+      switch (ret)
+      {
+      case HTTP_UPDATE_OK:
+        message = "Successfuly updated!";
+        Serial.println(message);
+        break;
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        message = "Update failed: " + String(ESPhttpUpdate.getLastErrorString());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        message = "No updates";
+        Serial.println(message);
+        break;
+      }
+    }
+    else
+    {
+      message = "Already on latest version";
+      Serial.println(message);
+    }
+  }
+  else
+  {
+    message = "Version check failed, http code: " + String(httpCode) + " ,message: " + httpClient.errorToString(httpCode);
+    Serial.println(message);
+  }
+  httpClient.end();
+
+  terminal.println(message);
+  terminal.flush();
+}
 
 void setup()
 {
@@ -43,6 +154,7 @@ void setup()
     Blynk.begin(settings.blynkAuth, settings.wifiSSID, settings.wifiPassword);
     Blynk.virtualWrite(V1, value);
     Blynk.virtualWrite(V2, WiFi.RSSI());
+    Blynk.virtualWrite(V4, settings.version);
 
     Serial.println("average:\t" + String(value) + " kg");
   }
@@ -53,6 +165,14 @@ void setup()
 
   scale.power_down();
   delay(300);
+
+  if (startOTA)
+  {
+    startOTA = false;
+    checkForUpdates();
+  }
+
+  Blynk.disconnect();
   ESP.deepSleep(DEEP_SLEEP_TIME);
 }
 
